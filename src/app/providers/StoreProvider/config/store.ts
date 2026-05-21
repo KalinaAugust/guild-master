eebea2b22caa6b6ca55cbe0c1dedf414dfcf49db
Kalinin Denis <kalininaugust@gmail.com>
2026-05-21 21:35:40 +0300

    feat(store): register eventDetail reducer

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import { calendarReducer } from '@/entities/calendar';
 import { eventsReducer } from '@/entities/event';
 import { guildReducer } from '@/entities/guild/model/slice';
+import eventDetailReducer from '@/features/event-detail/model/slice';
 
 export const store = configureStore({
   reducer: {
     ui: calendarReducer,
     events: eventsReducer,
     guild: guildReducer,
+    eventDetail: eventDetailReducer,
   },
 });
 
