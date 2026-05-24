1496e6021fa97c548f2718713f63bfef835f9f55
Kalinin Denis <kalininaugust@gmail.com>
2026-05-24 17:12:56 +0300

    feat(guild): add RTK Query endpoints for list, create, delete

--- src/entities/guild/index.ts
+++ src/entities/guild/index.ts
@@ -2,4 +2,9 @@ export * from './model/types';
 export * from './model/slice';
 export * from './api/getGuilds';
 export * from './api/createGuild';
-export { useGetGuildMembersQuery } from './api/guildApi';
+export {
+  useGetGuildMembersQuery,
+  useGetGuildsQuery,
+  useCreateGuildMutation,
+  useDeleteGuildMutation,
+} from './api/guildApi';
