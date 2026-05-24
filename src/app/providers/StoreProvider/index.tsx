6ab157a5742ab8a9fd06eb0a573b605f157845f9
Kalinin Denis <kalininaugust@gmail.com>
2026-05-24 15:47:13 +0300

    feat(app): add TooltipProvider to StoreProvider

--- src/app/providers/StoreProvider/index.tsx
+++ src/app/providers/StoreProvider/index.tsx
@@ -3,7 +3,14 @@
 import { Provider } from 'react-redux';
 import { store } from './config/store';
 import { ReactNode } from 'react';
+import * as TooltipPrimitive from '@radix-ui/react-tooltip';
 
 export default function StoreProvider({ children }: { children: ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <TooltipPrimitive.Provider>
+        {children}
+      </TooltipPrimitive.Provider>
+    </Provider>
+  );
 }
